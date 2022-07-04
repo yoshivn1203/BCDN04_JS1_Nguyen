f5f06c5a836440d100cfd11fb8e7c639dadcaf92
yoshi1203 <yoshivn1203@gmail.com>
2022-07-04 23:42:57 +0700

    chỉnh sửa

--- js/bai4.js
+++ js/bai4.js
@@ -18,4 +18,4 @@ let chieuDai = 15;
 let chieuRong = 10;
 
 console.log(`Diện tích hình chữ nhật: ${chieuDai * chieuRong}
-             Chu vi hình chữ nhật: ${(chieuDai + chieuRong) * 2}`);
+Chu vi hình chữ nhật: ${(chieuDai + chieuRong) * 2}`);
